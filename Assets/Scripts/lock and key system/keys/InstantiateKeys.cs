4b28adee8bb792c5cedd845dd82e1d178f0fdff5
ronBLAK <aaronvinu08@gmail.com>
2025-04-08 17:12:50 +1200

    deleted all inventory progress
    wanted to start the whole lock and key system from scratch, to work on it in a more planned and directed pathway

--- Assets/Scripts/lock and key system/keys/InstantiateKeys.cs
+++ Assets/Scripts/lock and key system/keys/InstantiateKeys.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class GreenKey : MonoBehaviour
+public class InstantiateKeys : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
